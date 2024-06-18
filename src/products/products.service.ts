c01e0cd276814c81da5222ad4a530ea06568273a
Alejandrolara21 <alejandroguacanemelara@gmail.com>
2024-06-18 14:09:16 -0500

    clean console

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -56,7 +56,6 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async update(id: number, updateProductDto: UpdateProductDto) {
     const { id: __, ...data } = updateProductDto;
-    console.log(__);
     await this.findOne(id);
     return this.product.update({
       where: {
